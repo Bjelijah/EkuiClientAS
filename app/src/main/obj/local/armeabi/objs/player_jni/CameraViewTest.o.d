747c158f96efeb216af1c8180a6faae73899d847
Bjelijah <elijah@live.cn>
2016-11-03 10:00:06 +0800

    Initial commit

---
+++ app/src/main/obj/local/armeabi/objs/player_jni/CameraViewTest.o.d
@@ -0,0 +1,18 @@
+./obj/local/armeabi/objs/player_jni/CameraViewTest.o: \
+  jni/CameraViewTest.cpp jni/include/play_def.h jni/include/hw_config.h \
+  jni/include/stream_type.h jni/include/net_sdk.h jni/include/net_err.h \
+  jni/include/protocol_type.h jni/com_example_utils_JniUtil.h
+
+jni/include/play_def.h:
+
+jni/include/hw_config.h:
+
+jni/include/stream_type.h:
+
+jni/include/net_sdk.h:
+
+jni/include/net_err.h:
+
+jni/include/protocol_type.h:
+
+jni/com_example_utils_JniUtil.h:
